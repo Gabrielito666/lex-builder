ac36dbefe119673acad9ef0ddee226bcc6fc3bff
Gabrielito666 <galileo.fandango@gmail.com>
2024-11-10 22:27:40 -0300

    Debug

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const getNewBundle = require('./lib/hydrat');
 
 const esbuild = require('esbuild');
 const fs = require("fs");
-const htmlTemplate = require('./lib/html-template');
 const buildHtml = require('./lib/build-html');
 const serialice = require('./lib/html-template');
+const path = require('path');
 
-const lexDevPlugin = (outputPath, entryPoint, htmlBase) => 
+const lexDevPlugin = (outputPath) => 
 ({
     name: "lex-dev-plugin",
     setup(build)
@@ -16,21 +16,24 @@ const lexDevPlugin = (outputPath, entryPoint, htmlBase) =>
             if (result.errors.length > 0) return result.errors;
             for(let output of result.outputFiles)
             {   
-                const html = getNewBundle(output, entryPoint, htmlBase);
+                const html = getNewBundle(output);
                 fs.writeFileSync(outputPath, html);
             }
         })
     }
 })
 
-const buildDev = async(layout, page, output) =>
+const buildDev = async(page, layout, output) =>
 {
+    const resolveDir = path.dirname(page);
     return esbuild.build
     ({
         stdin:
         {
             contents: serialice(page, layout),
             loader: 'js',
+            resolveDir,
+            loader: "jsx"
         },
         bundle: true,
         minify: false,
@@ -38,7 +41,7 @@ const buildDev = async(layout, page, output) =>
         jsxFactory: "Lex.createElement",
         jsxFragment: "Lex.fragment",
         write: false,
-        plugins : [lexDevPlugin(output, entryPoint, htmlBase)]
+        plugins : [lexDevPlugin(output)]
     })
 }
 
