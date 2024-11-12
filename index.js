b920eea5be1a0a23998c264ce5c8830a756c1125
Gabrielito666 <galileo.fandango@gmail.com>
2024-11-12 15:22:14 -0300

    production builds

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const getNewBundle = require('./lib/hydrat');
+const { minify } = require('html-minifier')
 
 const esbuild = require('esbuild');
 const fs = require("fs");
-const buildHtml = require('./lib/build-html');
+const buildHtml_ = require('./lib/build-html');
 const serialice = require('./lib/html-template');
 const path = require('path');
 
-const lexDevPlugin = (outputPath) => 
+const lexDevPlugin = (outputPath, { mode="dev" }) => 
 ({
     name: "lex-dev-plugin",
     setup(build)
@@ -16,7 +17,16 @@ const lexDevPlugin = (outputPath) =>
             if (result.errors.length > 0) return result.errors;
             for(let output of result.outputFiles)
             {   
-                const html = getNewBundle(output);
+                const html = mode == "production" ?
+                minify(getNewBundle(output),
+                {
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    removeRedundantAttributes: true,
+                    removeEmptyAttributes: true,
+                }) :
+                getNewBundle(output);
+
                 fs.writeFileSync(outputPath, html);
             }
         })
@@ -46,12 +56,39 @@ const buildDev = async(page, layout, output) =>
 }
 
 
-const buildHtmlDev = async(entryPoints, output) => 
-[...entryPoints].map(entryPoint =>
+const buildHtmlDev = async(entryPoint, output) => 
 {
-    const html = buildHtml(entryPoint);
+    const html = buildHtml_(entryPoint);
     fs.writeFileSync(output, html, 'utf-8')
     return output;
-});
+};
 
-module.exports = { buildDev, buildHtmlDev };
+const build = (page, layout, output) =>
+{
+    const resolveDir = path.dirname(page);
+    return esbuild.build
+    ({
+        stdin:
+        {
+            contents: serialice(page, layout),
+            loader: 'js',
+            resolveDir,
+            loader: "jsx"
+        },
+        bundle: true,
+        minify: true,
+        platform: "browser",
+        jsxFactory: "Lex.createElement",
+        jsxFragment: "Lex.fragment",
+        write: false,
+        plugins : [lexDevPlugin(output, { mode: 'production' })]
+    })
+}
+const buildHtml = (entryPoint, output) =>
+{
+    const html = buildHtml_(entryPoint, { mode: "production" });
+    fs.writeFileSync(output, html, 'utf-8');
+    return output;
+}
+
+module.exports = { buildDev, buildHtmlDev, build, buildHtml };
